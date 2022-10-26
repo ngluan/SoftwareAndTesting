8a9355b28b80a9ca595a13fb7792cd8ca60066fe
Fillip Novak Grunnsund <fillnova@hotmail.com>
2022-10-26 20:29:49 +0200

    feat: add user class

---
+++ src/main/java/models/user.java
@@ -0,0 +1,37 @@
+package models;
+
+public class user {
+    int id;
+    String name;
+    int age;
+
+    public user(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
