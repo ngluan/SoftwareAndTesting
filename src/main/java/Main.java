d84614fbb14dfdae4ff5f401af7fe3f3ec602071
Luan <globe_909@hotmail.com>
2022-10-26 18:44:58 +0200

    Write to json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 import java.io.File;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
-
 
 public class Main {
     public static void main(String[] args) {
