aef4b478d4faa1e19ed3e4d3c7682d12a7e0ef2d
Luan <globe_909@hotmail.com>
2022-10-26 20:06:50 +0200

    Add a test to test the test of the test.

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,4 +1,11 @@
-package java;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class Testing {
+
+    @Test
+    public void Test_true_or_false(){
+        boolean isFalse = false;
+        assertFalse(isFalse);
+    }
 }
